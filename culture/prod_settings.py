84f93c82a06be2204dff7361dc788df5167bd212
TML <tsoataevrad7@gmail.com>
2023-04-29 17:45:43 +0100

    Modification du fichier de configuration de settings production

---
+++ culture/prod_settings.py
@@ -0,0 +1,12 @@
+
+import dj_database_url
+from culture.settings import *
+# DEBUG=False
+# TEMPLATE_DEBUG=False
+ALLOWED_HOSTS = [
+    "cameroon-culture.herokuapp.com",
+    "127.0.0.1",
+]
+
+SECRET_KEY = '*&zegul5y2+ho#w#2=xl3!)&1zhz5d$u8#%8o*a8q^=-1agf50'
+DATABASES["default"] = dj_database_url.config()
