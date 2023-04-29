6900392f50184db31d6b980e85f647039a621213
TML <tsoataevrad7@gmail.com>
2023-04-29 18:15:42 +0100

    Configuration des fichiers statiques en production

--- culture/prod_settings.py
+++ culture/prod_settings.py
@@ -10,3 +10,10 @@ ALLOWED_HOSTS = [
 
 SECRET_KEY = '*&zegul5y2+ho#w#2=xl3!)&1zhz5d$u8#%8o*a8q^=-1agf50'
 DATABASES["default"] = dj_database_url.config()
+
+MIDDLEWARE = [
+    MIDDLEWARE[0],
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+].extend(MIDDLEWARE[2:])
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
