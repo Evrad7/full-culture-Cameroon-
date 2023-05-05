356df5d3b0477511d1283aca936917befaf76a82
TML <tsoataevrad7@gmail.com>
2023-05-05 11:04:54 +0100

    Protection des clés sécretes

--- culture/prod_settings.py
+++ culture/prod_settings.py
@@ -1,6 +1,7 @@
 
 import dj_database_url
 from culture.settings import *
+import os
 DEBUG = False
 TEMPLATE_DEBUG = False
 ALLOWED_HOSTS = [
@@ -8,7 +9,8 @@ ALLOWED_HOSTS = [
     "127.0.0.1",
 ]
 
-SECRET_KEY = '*&zegul5y2+ho#w#2=xl3!)&1zhz5d$u8#%8o*a8q^=-1agf50'
+SECRET_KEY = os.environ["SECRET_KEY"]
+
 DATABASES["default"] = dj_database_url.config()
 
 
@@ -30,7 +32,6 @@ COMPRESS_OFFLINE = True
 
 
 def get_cache():
-    import os
     try:
         servers = os.environ['MEMCACHIER_SERVERS']
         username = os.environ['MEMCACHIER_USERNAME']
@@ -61,10 +62,7 @@ CACHES = get_cache()
 
 INSTALLED_APPS.append("storages")
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-AWS_S3_ACCESS_KEY_ID = "AKIAXD6D5GSQD3M7QNDQ"
-AWS_S3_SECRET_ACCESS_KEY = "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"
+AWS_S3_ACCESS_KEY_ID = os.environ["AWS_S3_ACCESS_KEY_ID"]
+AWS_S3_SECRET_ACCESS_KEY = os.environ["AWS_S3_SECRET_ACCESS_KEY"]
 AWS_STORAGE_BUCKET_NAME = "cameroon-culture"
 AWS_S3_REGION_NAME = "eu-north-1"
-
-# Clé d'access = AKIAXD6D5GSQD3M7QNDQ
-# Clé d'acces sécrete=XKn488Es6iljmNc74Hg7P7QMzDISWLt1xDZxcUVr
